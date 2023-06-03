438a06e1cde839bcbaa0714026f809feadf3227c
JacobYooHandsome <tolearneverything123@gmail.com>
2023-06-02 21:49:01 -0400

    Added core functionality of finding the deals

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from flight_search import FlightSearch
+from data_manager import DataManager
+from notification_manager import NotificationManager
 
+TOMMORROW = datetime.now() + timedelta(1)
+SIX_MONTHS = datetime.now() + relativedelta(months=+6)
+MY_LOCATION = "ATL"
 
-# testing 123
+flightSearch = FlightSearch()
+dataManager = DataManager()
+notificationManager = NotificationManager()
+
+codes = flightSearch.find_IATAs(dataManager.cities)
+dataManager.update_IATAs(codes)
+
+for i in range(len(dataManager.cities)):
+    search_params = {
+            "fly_from": MY_LOCATION,
+            "fly_to": codes[i],
+            "date_from": TOMMORROW.strftime('%d/%m/%Y'),
+            "date_to": SIX_MONTHS.strftime('%d/%m/%Y'),
+            "price_to": int(dataManager.prices[i]),
+            "curr": "USD",
+            "sort": "price"
+        }
+    search_data = flightSearch.find_deals(search_params)
+    if search_data["_results"] > 0:
+        
+        cheapest_flight = search_data["data"][0]
+        
+        price = cheapest_flight["price"]
+        city_from = cheapest_flight["cityFrom"]
+        code_from = cheapest_flight["cityCodeFrom"]
+        city_to = cheapest_flight["cityTo"]
+        code_to = cheapest_flight["cityCodeTo"]
+        departure = datetime.strptime(f"{cheapest_flight['utc_departure'].replace('.000Z', '')}", "%Y-%m-%dT%H:%M:%S").strftime("%Y/%m/%d")
+        arrival = datetime.strptime(f"{cheapest_flight['utc_arrival'].replace('.000Z', '')}", "%Y-%m-%dT%H:%M:%S").strftime("%Y/%m/%d")
+        link = cheapest_flight["deep_link"]
+        subject = f"GREAT FLIGHT DEAL FOR {city_to}!"
+        body = f"Only ${price} to fly from {city_from}-{code_from} to {city_to}-{code_to}, from {departure} to {arrival}!\n This is the link: \n{link}"
+        notificationManager.send_notification(subject=subject, body=body)
+    
--- notification_manager.py
+++ notification_manager.py
@@ -1,3 +1,23 @@
+import os
+from dotenv import load_dotenv
+from email.message import EmailMessage
+import ssl
+import smtplib
+load_dotenv()
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self):
+        self.email = os.getenv("EMAIL")
+        self.password = os.getenv("EMAIL_PASS")
+        self.emailObject = EmailMessage()
+    
+    def send_notification(self, subject, body):
+        self.emailObject.clear()
+        self.emailObject['From'] = self.email
+        self.emailObject['To'] = self.email
+        self.emailObject["Subject"] = subject
+        self.emailObject.set_content(body)
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(self.email, self.password)
+            smtp.sendmail(self.email, self.email, self.emailObject.as_string())
